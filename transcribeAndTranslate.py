4f654ada9d2edb10a4b6253cff2a8821d1694267
Krishna Agarwal <kagarwal@br005.pvt.bridges.psc.edu>
2019-09-11 19:32:31 -0400

    New file

---
+++ transcribeAndTranslate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+
+
+def transcribe_translate(sequence):
+    coding_dna = Seq(sequence, IUPAC.unambiguous_dna)
+    print(f"Original Sequence   : {sequence}")
+    print(f"Transcribe Sequence : {coding_dna.transcribe()}")
+    print(f"Translated Sequence : {coding_dna.translate()}")
+    print("\n")
+
+
+def main():
+    for sequence in ["ATGATTGGCCCGGTTTTTTAA", "GTGGTGGGGAAATTCCGCTGA"]:
+        transcribe_translate(sequence)
+
+
+if __name__ == '__main__':
+    main()
+
+
