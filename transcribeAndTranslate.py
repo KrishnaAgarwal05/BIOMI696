64f6c335b772d0a446d0bf7acfc866a0c21ce3a9
Krishna Agarwal <kagarwal@br018.pvt.bridges.psc.edu>
2019-09-13 18:46:40 -0400

    Added comments to transcribeAndTranslate.py file

--- transcribeAndTranslate.py
+++ transcribeAndTranslate.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+#Loading the required modules
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 
-
+#Definition for transcribing and translating
 def transcribe_translate(sequence):
     coding_dna = Seq(sequence, IUPAC.unambiguous_dna)
     print(f"Original Sequence   : {sequence}")
@@ -12,12 +13,12 @@ def transcribe_translate(sequence):
     print(f"Translated Sequence : {coding_dna.translate()}")
     print("\n")
 
-
+#Main definition
 def main():
     for sequence in ["ATGATTGGCCCGGTTTTTTAA", "GTGGTGGGGAAATTCCGCTGA"]:
         transcribe_translate(sequence)
 
-
+#Invokes the main definition
 if __name__ == '__main__':
     main()
 
